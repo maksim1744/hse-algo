3e10aff938296c2fecf8999c16eea1a09b20b614
Maksim Gorokhovskiy <magor1744@gmail.com>
2019-03-18 13:40:47 +0300

    upper case for constants and braces for one-line if/for

--- hash_map.cpp
+++ hash_map.cpp
@@ -11,40 +11,46 @@ class HashMap {
 
     HashMap(Hash hash = Hash())
           : hash(hash),
-            pointers(initial_size),
-            states(initial_size, state_empty) {
+            pointers(INITIAL_SIZE),
+            states(INITIAL_SIZE, STATE_EMPTY) {
     }
 
     template<class InputIt>
     HashMap(InputIt first, InputIt last, Hash hash = Hash())
           : hash(hash),
-            pointers(initial_size),
-            states(initial_size, state_empty) {
-        for (auto it = first; it != last; ++it)
+            pointers(INITIAL_SIZE),
+            states(INITIAL_SIZE, STATE_EMPTY) {
+        for (auto it = first; it != last; ++it) {
             insert(*it);
+        }
     }
 
     HashMap(std::initializer_list<std::pair<const KeyType, ValueType> > init, Hash hash = Hash())
           : hash(hash),
-            pointers(initial_size),
-            states(initial_size, state_empty) {
-        for (auto& pair : init)
+            pointers(INITIAL_SIZE),
+            states(INITIAL_SIZE, STATE_EMPTY) {
+        for (auto& pair : init) {
             insert(pair);
+        }
     }
 
     HashMap(const HashMap& other, Hash hash = Hash())
           : hash(hash),
-            pointers(initial_size),
-            states(initial_size, state_empty) {
-        for (auto& pair : other.values)
+            pointers(INITIAL_SIZE),
+            states(INITIAL_SIZE, STATE_EMPTY) {
+        for (auto& pair : other.values) {
             insert(pair);
+        }
     }
 
     HashMap& operator=(const HashMap& other) {
-        if (this == &other) return *this;
+        if (this == &other) {
+            return *this;
+        }
         clear();
-        for (auto& pair : other.values)
+        for (auto& pair : other.values) {
             insert(pair);
+        }
         return *this;
     }
 
@@ -74,11 +80,11 @@ class HashMap {
     Hash hash_function() const { return hash; }
 
  private:
-    const size_t occupancy_coefficient = 4;
-    const size_t initial_size = 8;
-    const char state_empty = 0;
-    const char state_filled = 1;
-    const char state_erased = 2;
+    const size_t OCCUPANCY_COEFFICIENT = 4;
+    const size_t INITIAL_SIZE = 8;
+    const char STATE_EMPTY = 0;
+    const char STATE_FILLED = 1;
+    const char STATE_ERASED = 2;
 
     Hash hash;
     std::vector<typename std::list<std::pair<const KeyType, ValueType> >::iterator> pointers;
@@ -94,35 +100,40 @@ template<class KeyType, class ValueType, class Hash>
 void HashMap<KeyType, ValueType, Hash>::insert(std::pair<KeyType, ValueType>&& pair) {
     check_size();
     size_t position = hash(pair.first) % pointers.size();
-    while (states[position] == state_filled) {
-        if (pointers[position]->first == pair.first)
+    while (states[position] == STATE_FILLED) {
+        if (pointers[position]->first == pair.first) {
             return;
+        }
         ++position;
-        if (position >= pointers.size())
+        if (position >= pointers.size()) {
             position = 0;
+        }
     }
     values.insert(end(), pair);
     pointers[position] = --end();
-    if (states[position] == state_empty)
+    if (states[position] == STATE_EMPTY) {
         ++occupancy;
-    states[position] = state_filled;
+    }
+    states[position] = STATE_FILLED;
 }
 
 template<class KeyType, class ValueType, class Hash>
 ValueType& HashMap<KeyType, ValueType, Hash>::operator[] (const KeyType& key) {
     check_size();
     size_t position = hash(key) % pointers.size();
-    while (states[position] == state_erased || (states[position] == state_filled && !(pointers[position]->first == key))) {
+    while (states[position] == STATE_ERASED || (states[position] == STATE_FILLED && !(pointers[position]->first == key))) {
         ++position;
-        if (position >= pointers.size())
+        if (position >= pointers.size()) {
             position = 0;
+        }
     }
-    if (states[position] != state_filled) {
+    if (states[position] != STATE_FILLED) {
         values.insert(end(), std::make_pair(key, ValueType()));
         pointers[position] = --end();
-        if (states[position] == state_empty)
+        if (states[position] == STATE_EMPTY) {
             ++occupancy;
-        states[position] = state_filled;
+        }
+        states[position] = STATE_FILLED;
     }
     return pointers[position]->second;
 }
@@ -130,53 +141,60 @@ ValueType& HashMap<KeyType, ValueType, Hash>::operator[] (const KeyType& key) {
 template<class KeyType, class ValueType, class Hash>
 const ValueType& HashMap<KeyType, ValueType, Hash>::at(const KeyType& key) const {
     size_t position = hash(key) % pointers.size();
-    while (states[position] == state_erased || (states[position] == state_filled && !(pointers[position]->first == key))) {
+    while (states[position] == STATE_ERASED || (states[position] == STATE_FILLED && !(pointers[position]->first == key))) {
         ++position;
-        if (position >= pointers.size())
+        if (position >= pointers.size()) {
             position = 0;
+        }
     }
-    if (states[position] != state_filled)
+    if (states[position] != STATE_FILLED) {
         throw std::out_of_range("");
+    }
     return pointers[position]->second;
 }
 
 template<class KeyType, class ValueType, class Hash>
 void HashMap<KeyType, ValueType, Hash>::erase(const KeyType& key) {
     size_t position = hash(key) % pointers.size();
-    while (states[position] == state_erased || (states[position] == state_filled && !(pointers[position]->first == key))) {
+    while (states[position] == STATE_ERASED || (states[position] == STATE_FILLED && !(pointers[position]->first == key))) {
         ++position;
-        if (position >= pointers.size())
+        if (position >= pointers.size()) {
             position = 0;
+        }
     }
-    if (states[position] == state_filled) {
+    if (states[position] == STATE_FILLED) {
         values.erase(pointers[position]);
-        states[position] = state_erased;
+        states[position] = STATE_ERASED;
     }
 }
 
 template<class KeyType, class ValueType, class Hash>
 typename HashMap<KeyType, ValueType, Hash>::iterator HashMap<KeyType, ValueType, Hash>::find(const KeyType& key) {
     size_t position = hash(key) % pointers.size();
-    while (states[position] == state_erased || (states[position] == state_filled && !(pointers[position]->first == key))) {
+    while (states[position] == STATE_ERASED || (states[position] == STATE_FILLED && !(pointers[position]->first == key))) {
         ++position;
-        if (position >= pointers.size())
+        if (position >= pointers.size()) {
             position = 0;
+        }
     }
-    if (states[position] != state_filled)
+    if (states[position] != STATE_FILLED) {
         return end();
+    }
     return pointers[position];
 }
 
 template<class KeyType, class ValueType, class Hash>
 typename HashMap<KeyType, ValueType, Hash>::const_iterator HashMap<KeyType, ValueType, Hash>::find(const KeyType& key) const {
     size_t position = hash(key) % pointers.size();
-    while (states[position] == state_erased || (states[position] == state_filled && !(pointers[position]->first == key))) {
+    while (states[position] == STATE_ERASED || (states[position] == STATE_FILLED && !(pointers[position]->first == key))) {
         ++position;
-        if (position >= pointers.size())
+        if (position >= pointers.size()) {
             position = 0;
+        }
     }
-    if (states[position] != state_filled)
+    if (states[position] != STATE_FILLED) {
         return end();
+    }
     return pointers[position];
 }
 
@@ -184,11 +202,12 @@ template<class KeyType, class ValueType, class Hash>
 void HashMap<KeyType, ValueType, Hash>::clear() {
     for (auto& pair : values) {
         size_t position = hash(pair.first) % pointers.size();
-        while (states[position] != state_empty) {
-            states[position] = state_empty;
+        while (states[position] != STATE_EMPTY) {
+            states[position] = STATE_EMPTY;
             ++position;
-            if (position >= pointers.size())
+            if (position >= pointers.size()) {
                 position = 0;
+            }
         }
     }
     values.clear();
@@ -197,19 +216,20 @@ void HashMap<KeyType, ValueType, Hash>::clear() {
 
 template<class KeyType, class ValueType, class Hash>
 void HashMap<KeyType, ValueType, Hash>::check_size() {
-    if (occupancy * occupancy_coefficient >= pointers.size()) {
+    if (occupancy * OCCUPANCY_COEFFICIENT >= pointers.size()) {
         try {
             std::vector<typename std::list<std::pair<const KeyType, ValueType> >::iterator> new_pointers(pointers.size() * 2);
-            states.assign(new_pointers.size(), state_empty);
+            states.assign(new_pointers.size(), STATE_EMPTY);
             for (auto it = begin(); it != end(); ++it) {
                 size_t position = hash(it->first) % new_pointers.size();
-                while (states[position] == state_filled) {
+                while (states[position] == STATE_FILLED) {
                     ++position;
-                    if (position >= new_pointers.size())
+                    if (position >= new_pointers.size()) {
                         position = 0;
+                    }
                 }
                 new_pointers[position] = it;
-                states[position] = state_filled;
+                states[position] = STATE_FILLED;
                 occupancy = 0;
             }
             std::swap(pointers, new_pointers);
